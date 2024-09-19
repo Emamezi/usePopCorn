7653fa189ab6e1245acd2b9ef95e11ee9fd3c7a2
Mezi <emameziebebinwe@gmail.com>
2024-09-19 09:58:42 -0400

    addded query seach focus upon eneter key pres feature

--- src/components/search.component.js
+++ src/components/search.component.js
@@ -1,4 +1,19 @@
+import { useEffect, useRef } from "react";
+
 const Search = ({ query, setQuery }) => {
+  const inputEl = useRef(null);
+  useEffect(() => {
+    function callBack(e) {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener("keydown", callBack);
+    //clean u function
+    return () => document.removeEventListener("keydown", callBack);
+  }, [setQuery]);
   return (
     <input
       className="search"
@@ -6,6 +21,7 @@ const Search = ({ query, setQuery }) => {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 };
